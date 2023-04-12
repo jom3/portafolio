9acda814bac7cb103ca1c0e8a0a95d0cc9b65699
joams mogro <joamsmg@gmail.com>
2023-04-11 20:09:27 -0400

    about component done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', loadComponent: ()=>import('./components/navbar/navbar.component').then(c=>c.NavbarComponent), outlet:'navbar'
+  },
+  {
+    path:'', loadComponent: ()=>import('./components/about/about.component').then(c=>c.AboutComponent), outlet:'about'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
